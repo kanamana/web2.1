a47247e4a15c9031571a4ab08ea0e2b32bab8753
kanamana <80589804+kanamana@users.noreply.github.com>
2021-03-13 22:57:33 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,10 +2,10 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-#task 1.9 [11]
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo rm /etc/gunicorn.d/hello.py
+sudo rm /etc/gunicorn.d/django.py
 
-#task 2.1
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/django.py
 
 sudo /etc/init.d/gunicorn restart
