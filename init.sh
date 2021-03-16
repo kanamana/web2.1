37876d6d06049b439886d8bb2b44e8317db0aa4d
kanamana <80589804+kanamana@users.noreply.github.com>
2021-03-16 10:05:06 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,22 +1,6 @@
-sudo apt-get update
-sudo apt-get install -y python3.5
-sudo apt-get install -y python3.5-dev
-sudo unlink /usr/bin/python3
-sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-sudo pip3 install --upgrade pip
-sudo pip3 install --upgrade django==2.1
-sudo pip3 install --upgrade gunicorn
-
-#sudo pip install -r /home/box/web/requirements.txt 
-#sudo pip3 install --upgrade django
-
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo unlink /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
 sudo /etc/init.d/nginx restart
-
-sudo rm /etc/gunicorn.d/hello.py
-sudo rm /etc/gunicorn.d/django.py
-
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/django.py
-
-sudo /etc/init.d/gunicorn restart
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_get_query_pars &
+sudo gunicorn -c /home/box/web/etc/ask.py ask.wsgi &
