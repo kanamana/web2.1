30ba6c9d406a4941fbd958f110bc3496f53e71ce
kanamana <80589804+kanamana@users.noreply.github.com>
2021-03-16 10:20:12 +0600

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,26 +1,15 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 from django.contrib import admin
+from qa import views
 
-from qa.views import test, popular, question_detail, new, question_ask, question_answer_add
-from qa.views import user_signup, user_login, user_logout
-admin.autodiscover()
+urlpatterns = [
+    url(r'^$', views.test),
+    url(r'^login/', views.test),
+    url(r'^signup/', views.test),
+    url(r'^question/\d+/', views.test),
+    url(r'^ask/', views.test),
+    url(r'^popular/', views.test),
+    url(r'^new/', views.test),
+    url(r'^admin/', admin.site.urls),
+]
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ask.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    #url(r'^$', question_list_all, name='question-list-all'),
-    url(r'^$', new, name='index'),
-    url(r'^login/', user_login, name='user_login'),
-    url(r'^logout/', user_logout, name='user_logout'),
-    url(r'^signup/', user_signup, name='user_signup'),
-    url(r'^question/(?P<pk>\d+)/', question_detail, name='question_detail'),
-    url(r'^answer/', question_answer_add, name='question_answer'),
-    url(r'^ask/', question_ask, name='question_ask'),
-    url(r'^popular/', popular, name='popular'),
-    url(r'^new/', new, name='new'),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
